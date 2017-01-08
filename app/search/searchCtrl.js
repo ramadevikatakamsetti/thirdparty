f65b0b4ef765db427907b209b42237b5932d63b2
ramadevikatakamsetti <ishwaryanidhik12@gmail.com>
2017-01-08 18:10:13 +0530

    new code

--- app/search/searchCtrl.js
+++ app/search/searchCtrl.js
@@ -1,18 +1,21 @@
 (function(){
     function searchCtrlFn($scope,searchSvc){
-        $scope.state="";
-        $scope.country="";
+      //  $scope.state="";
+      //  $scope.country="";
         $scope.visible=false;
         
         $scope.data=function(){
             $scope.visible=true;
-           var country=$scope.country;
-            var state=$scope.state;
-            console.log(country);
-            console.log(state);
-           searchSvc.getWeatherDetails($scope.country, $scope.state)
+         //  var country=$scope.country;
+//var state=$scope.state;
+         //   console.log(country);
+         //   console.log(state);
+//searchSvc.getWeatherDetails($scope.country, $scope.state)
+            searchSvc.getWeatherDetails()
             .then(function(res){
-                 $scope.searchData=res.data.query.results.channel;
+                // $scope.searchData=res.data.query.results.channel;
+                 $scope.searchData=res.data;
+                 
                console.log($scope.searchData);
             })
                .catch(function(error){
--- app/search/searchSvc.js
+++ app/search/searchSvc.js
@@ -3,8 +3,9 @@
         
         this.getWeatherDetails= function(country,state){
             var dfd=$q.defer();
-             var weatherApi = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + country + '%2C%20' + state + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+          //   var weatherApi = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + country + '%2C%20' + state + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
 
+                 var weatherApi='https://api.github.com/users/hadley/orgs';
             $http.get(weatherApi)
                 .then(function(response){
                       dfd.resolve(response);
